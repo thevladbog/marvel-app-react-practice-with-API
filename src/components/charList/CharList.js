692cdb28d51aab92670cfac6d62396060f5c904e
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2022-01-26 22:26:55 +0300

    fix: highlit selected Char in CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
+
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelService";
@@ -11,7 +13,7 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     offset: 210,
-    sharEnded: false,
+    charEnded: false,
   };
 
   marvelService = new MarvelService();
@@ -40,7 +42,7 @@ class CharList extends Component {
       ended = true;
     }
 
-    this.setState(({ charList, offset }) => ({
+    this.setState(({ offset, charList }) => ({
       charList: [...charList, ...newCharList],
       loading: false,
       newItemLoading: false,
@@ -56,8 +58,31 @@ class CharList extends Component {
     });
   };
 
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    // Я реализовал вариант чуть сложнее, и с классом и с фокусом
+    // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+    // На самом деле, решение с css-классом можно сделать, вынеся персонажа
+    // в отдельный компонент. Но кода будет больше, появится новое состояние
+    // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+    // По возможности, не злоупотребляйте рефами, только в крайних случаях
+    this.itemRefs.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRefs[id].classList.add("char__item_selected");
+    this.itemRefs[id].focus();
+  };
+
+  // Этот метод создан для оптимизации,
+  // чтобы не помещать такую конструкцию в метод render
   renderItems(arr) {
-    const items = arr.map((item) => {
+    const items = arr.map((item, i) => {
       let imgStyle = { objectFit: "cover" };
       if (
         item.thumbnail ===
@@ -69,15 +94,26 @@ class CharList extends Component {
       return (
         <li
           className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
         </li>
       );
     });
-
+    // А эта конструкция вынесена для центровки спиннера/ошибки
     return <ul className="char__grid">{items}</ul>;
   }
 
@@ -108,5 +144,9 @@ class CharList extends Component {
     );
   }
 }
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
